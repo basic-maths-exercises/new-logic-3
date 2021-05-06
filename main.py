7da2ed20b323543ab9b621e87a797b5468b9a734
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-06 16:51:03 +0100

    Added answer

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ import numpy as np
 
 # This loads the data from values.dat into the NumPy array called xvals
 xvals = np.loadtxt("mydata.dat")
+yvals = np.zeros(len(xvals))
+for i,xv in enumerate(xvals) :
+    if xv==5 : yvals[i] = 1
